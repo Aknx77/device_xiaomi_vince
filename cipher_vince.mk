905dfc0eee0fb50e899a332f359d9c2a425fc040
Aditya N <nayakaditya0530@gmail.com>
2022-03-21 18:33:50 +0530

    vince: Update Redfin FP to March
    Signed-off-by: Aditya N <nayakaditya0530@gmail.com>

--- cipher_vince.mk
+++ cipher_vince.mk
@@ -48,7 +48,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220205.002 8010174 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SP2A.220305.012 8177914 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys"
